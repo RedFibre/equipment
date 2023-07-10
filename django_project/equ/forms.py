88295c6d6a75a90aab896f7e7ef1f1705c3cdf34
DevanshUpadhyay26 <Devanshupadhyay26@gmail.com>
2023-07-10 16:54:15 +0530

    @DevanshUpadhyay26 added UI and widgets

--- django_project/equ/forms.py
+++ django_project/equ/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Project, Equipment, Booking,Material
-from django.forms import inlineformset_factory,formset_factory
+from django.forms import inlineformset_factory,formset_factory,DateTimeInput, DateInput
 
 
 
@@ -10,7 +10,10 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['user','lab']
-
+        widgets = {
+            'start_date': DateInput(attrs={'type': 'date'}),
+            'end_date': DateInput(attrs={'type': 'date'}),
+        }
 
 class BookingForm(forms.ModelForm):
     materials = forms.ModelMultipleChoiceField(queryset=Material.objects.none(), widget=forms.CheckboxSelectMultiple)
@@ -18,7 +21,9 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['equipment','start_time', 'end_time', 'materials']
         widgets = {
-            'equipment': forms.Select(choices=())
+            'equipment': forms.Select(choices=()),
+            'start_time': DateTimeInput(format='%d/%m/%Y %I:%M %p'),
+            'end_time': DateTimeInput(format='%d/%m/%Y %I:%M %p')
         }
 
     def __init__(self, *args, **kwargs):
@@ -26,6 +31,8 @@ class BookingForm(forms.ModelForm):
         available_materials = kwargs.pop('available_materials')
         super().__init__(*args, **kwargs)
         self.fields['equipment'].widget.choices = [(equipment.id, equipment.name) for equipment in available_equipment]
+        self.fields['start_time'].widget = DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M')
+        self.fields['end_time'].widget = DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M')
         self.fields['materials'].queryset = available_materials
         
 BookingFormSet = formset_factory(BookingForm, extra=1) 
